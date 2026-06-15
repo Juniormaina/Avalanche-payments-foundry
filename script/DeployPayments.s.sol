d6a9cfd407f54509dd5a496ca92a32fa7614b1b1
Juniormaina <zangiffk@gmail.com>
2026-06-15 18:41:37 +0300

    Resolve .gitignore conflict during rebase
    :wq

--- script/DeployPayments.s.sol
+++ script/DeployPayments.s.sol
@@ -2,15 +2,15 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import "openzeppelin/contracts/finance/PaymentSplitter.sol";
-import "openzeppelin/contracts/finance/Escrow.sol";
+import "openzeppelin-contracts/contracts/finance/PaymentSplitter.sol";
+import "openzeppelin-contracts/contracts/finance/VestingWallet.sol";
 
 contract DeployPayments is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        // Example payees and shares
+        // --- Deploy PaymentSplitter ---
         address[] memory payees = new address[](2);
         payees[0] = 0x066d4646Ce97959fa45a933065946ED5A162E686;
         payees[1] = 0x7cb653d1c6E4Df222DE5106134707Ce2Eaa704A3;
@@ -20,7 +20,13 @@ contract DeployPayments is Script {
         shares[1] = 50;
 
         PaymentSplitter splitter = new PaymentSplitter(payees, shares);
-        Escrow escrow = new Escrow();
+
+        // --- Deploy VestingWallet ---
+        VestingWallet vesting = new VestingWallet(
+            0x066d4646Ce97959fa45a933065946ED5A162E686, // beneficiary
+            uint64(block.timestamp),                   // start time
+            365 days                                   // vesting duration
+        );
 
         vm.stopBroadcast();
     }
